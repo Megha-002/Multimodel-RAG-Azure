d67297c9e44eee838352586884d12dfead79af34
Meghana <meghanalopinti333@gmail.com>
2026-04-20 00:39:12 +0530

    feat: Addition of schemas.py it defines the data type rules for api callings

---
+++ apps/api/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class QueryRequest(BaseModel):
+    question: str
+
+class QueryResponse(BaseModel):
+    answer: str
+    sources: list[str]
+
+class STTResponse(BaseModel):
+    transcript: str
